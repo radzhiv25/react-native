cf3f698f4852e9c0ed92fa94a1c8ed63129a89d5
Rajeev Krishna <lambo2572@gmail.com>
2023-06-12 15:20:40 +0530

    Started ReactNative for Weather App

---
+++ WeatherApp/App.js
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{
+      color: 'white',
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <Text>Hello, This is rajeev 👋</Text>
+      <Text>I am front-end Developer</Text>
+    </View>
+  );
+}
+
